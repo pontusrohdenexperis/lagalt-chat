7a4a2ecb29e88b00229cc10f3bb04a269fc65f04
Pontus Rohden <pontus.rohden@se.experis.com>
2021-09-16 15:12:52 +0200

    forwardslash

--- lagalt-chat/server.js
+++ lagalt-chat/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://lagalt-frontend-gbg.herokuapp.com/",
+    origin: "https://lagalt-frontend-gbg.herokuapp.com",
     methods: ["GET", "POST"],
   },
 });
