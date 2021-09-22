0a76979729b75a0cb4bcf8496ac8a0a013e1800d
pontusrohdenexperis <88532449+pontusrohdenexperis@users.noreply.github.com>
2021-09-22 11:57:10 +0200

    Update server.js

--- lagalt-chat/server.js
+++ lagalt-chat/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://lagalt-frontend-gbg.herokuapp.com/",
+    origin: "https://lagalt-frontend-gbg.herokuapp.com/*",
     methods: ["GET", "POST"],
   },
 });
